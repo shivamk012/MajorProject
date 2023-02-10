c6956e2513031c1bb7ba3d24852935da60cc9465
shivamk012 <shivamkoolwal14@gmail.com>
2023-02-10 12:50:25 +0530

    new api

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const port = process.env.PORT || 3000;
 app.post('/tweeted', (req, res) => {
     const { body } = req;
     console.log(body.tweet);
-    res.send("Success");
+    res.send("Success in tweeted");
 });
+
+app.get('/' , (req,res)=>{
+    res.send("Success in home");
+})
 
 app.listen(port, () => {
     console.log(`Express api/webhook app listening at http://localhost:${port}`);
